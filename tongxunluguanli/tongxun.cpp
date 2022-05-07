7d4551c0519860b6ce64f1084375848f486fb374
Zoey-xue47 <450287421@qq.com>
2022-05-07 10:23:41 +0800

    add some changes

---
+++ tongxunluguanli/tongxun.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void showMenu(){
+
+    std::cout<<"***********************"<<std::endl;
+    std::cout<<"*****1、添加联系人*****"<<std::endl;
+    std::cout<<"*****2、显示联系人*****"<<std::endl;
+    std::cout<<"*****3、删除联系人*****"<<std::endl;
+    std::cout<<"*****4、查找联系人*****"<<std::endl;
+    std::cout<<"*****5、修改联系人*****"<<std::endl;
+    std::cout<<"*****6、清空联系人*****"<<std::endl;
+    std::cout<<"*****0、退出通讯录*****"<<std::endl;
+    std::cout<<"***********************"<<std::endl;
+
+
+}
+
+int main(){
+
+    showMenu();
+}
